ada6023f45265b393cba341be0a3f090100e0aa6
Aayush <kumar.aayush2310@gmail.com>
2023-07-02 11:47:50 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,47 @@
+import 'package:expense_tracker/firebase_options.dart';
+import 'package:expense_tracker/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
-main() => runApp(
-      const MaterialApp(
-        home: LandingScreen(),
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    const MaterialApp(
+      home: NewHome(),
+    ),
+  );
+}
+
+class NewHome extends StatelessWidget {
+  const NewHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Home Page",
+        ),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user!.emailVerified) {}
+              return const Text("Done");
+
+            default:
+              return const Text("Loading");
+          }
+        },
       ),
     );
+  }
+}
